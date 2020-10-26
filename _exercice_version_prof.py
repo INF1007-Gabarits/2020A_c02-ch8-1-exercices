c7f4e2a2d925289e7d8a8accaa33db4d10c117ba
chosson <>
2020-10-26 14:47:00 -0400

    Added in-class code.

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import json
+import csv
+import os
+import time
+import configparser
+
+import inputs
 import mido
 
 
 NOTES_PER_OCTAVE = 12
+DEFAULT_VELOCITY = 80
 
 
 def build_note_dictionaries(note_names, add_octave_no=True):
@@ -25,88 +33,107 @@ def build_note_dictionaries(note_names, add_octave_no=True):
 			name_to_midi[full_note_name] = midi_no if add_octave_no else midi_no % NOTES_PER_OCTAVE
 	return midi_to_name, name_to_midi
 
-def build_print_note_name_callback(midi_to_name):
-	# Fonction locale
-	# Soit message MIDI midi_msg, midi_msg.type donne le type, midi_msg.note donne la note
-	
-	# Dans mon callback :
-	def callback(midi_msg):
-		# Si j'ai un note_on et une vélocité > 0, alors j'affiche le nom de la note associée au numéro (grâce au dict midi_to_name)
-		if midi_msg.type == "note_on" and midi_msg.velocity > 0:
-			print(midi_to_name[midi_msg.note])
-	return callback
+def send_note_on(note_name, name_to_midi, midi_outputs):
+	msg = mido.Message("note_on", note=name_to_midi[note_name], velocity=DEFAULT_VELOCITY)
+	for o in midi_outputs:
+		o.send(msg)
 
-def build_print_chord_name_callback(chord_names_and_notes, name_to_midi):
-	# Construire le dictionnaire d'assocations entre état des notes et accord joué.
-	chords = {}
+def send_note_off(note_name, name_to_midi, midi_outputs):
+	msg = mido.Message("note_off", note=name_to_midi[note_name])
+	for o in midi_outputs:
+		o.send(msg)
 
-	# Par exemple, [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0] -> "Do majeur"
-	for name, notes in chord_names_and_notes.items():
-		chord_notes = [False] * 12
-		for note in notes:
-			chord_notes[name_to_midi[note] % NOTES_PER_OCTAVE] = True
-		chords[tuple(chord_notes)] = name
+def build_note_callbacks(note_name, name_to_midi, midi_outputs):
+	# Construire des callbacks pour bouton appuyé et relâché
+	def action_fn_pressed():
+		send_note_on(note_name, name_to_midi, midi_outputs)
+	def action_fn_released():
+		send_note_off(note_name, name_to_midi, midi_outputs)
+	return action_fn_pressed, action_fn_released
 
-	# Créez et retourner le callback
-	def callback(midi_msg):
-		global note_states
-		# Si une note est appuyée
-		if midi_msg.type == "note_on" and midi_msg.velocity > 0:
-			# Je met son élément correspondant dans l'état du clavier à True
-			note_states[midi_msg.note % NOTES_PER_OCTAVE] = True
-			note_states_tuple = tuple(note_states)
-			# Si accord connu
-			if note_states_tuple in chords:
-				# Affiche nom de l'accord
-				print(chords[note_states_tuple])
-		# Sinon si une note est relâchée
-		elif midi_msg.type == "note_off" or (midi_msg.type == "note_on" and midi_msg.velocity == 0):
-			# Idem mais à False
-			note_states[midi_msg.note % NOTES_PER_OCTAVE] = False
-	return callback
+def build_chord_callbacks(chord, chord_notes, name_to_midi, midi_outputs):
+	# Construire des callbacks pour bouton appuyé et relâché
+	def action_fn_pressed():
+		for note in chord_notes[chord]:
+			send_note_on(note, name_to_midi, midi_outputs)
+	def action_fn_released():
+		for note in chord_notes[chord]:
+			send_note_off(note, name_to_midi, midi_outputs)
+	return action_fn_pressed, action_fn_released
 
-note_states = [False] * 12
+def build_custom_action_callbacks(action_name, custom_actions, midi_outputs):
+	# Construire des callbacks pour bouton appuyé et relâché
+	pressed, released = None, None
+	if True in custom_actions[action_name] and custom_actions[action_name][True] is not None:
+		def action_fn_pressed():
+			custom_actions[action_name][True](midi_outputs)
+		pressed = action_fn_pressed
+	if False in custom_actions[action_name] and custom_actions[action_name][False] is not None:
+		def action_fn_released():
+			custom_actions[action_name][False](midi_outputs)
+		released = action_fn_released
+	return pressed, released
+
+def load_input_mappings(filename, name_to_midi, chord_notes, midi_outputs, custom_actions={}):
+	config = configparser.ConfigParser()
+	config.read(filename)
+	gamepad_section = config["gamepad"]
+
+	mappings = {}
+	for gamepad_input in gamepad_section:
+		action_name = gamepad_section[gamepad_input]
+		# Construire des callbacks pour l'action appropriée et l'ajouter au mapping.
+		pressed, released = None, None
+		if action_name in name_to_midi:
+			pressed, released = build_note_callbacks(action_name, name_to_midi, midi_outputs)
+		elif action_name in chord_notes:
+			pressed, released = build_chord_callbacks(action_name, chord_notes, name_to_midi, midi_outputs)
+		elif action_name in custom_actions:
+			pressed, released = build_custom_action_callbacks(action_name, custom_actions, midi_outputs)
+		mappings[gamepad_input] = {True: pressed, False: released}
+
+	return mappings
 
 
 def main():
-	PORT_MIDI = "UM-ONE 0"
+	gamepad = inputs.devices.gamepads[0]
+	midi_outputs = (mido.open_output("UM-ONE 3"), mido.open_output("UnPortMIDI 4"))
+	midi_input = mido.open_input("UM-ONE 0")
 
-	english_names = ["C", "Db", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B"]
-	solfeggio_names = ["Do", "Réb", "Ré", "Mib", "Mi", "Fa", "Fa#", "Sol", "Lab", "La", "Sib", "Si"]
+	notes_data = json.load(open("notes.json", "r", encoding="utf-8"))
+	note_names = notes_data["solfeggio_names"]
+	midi_to_name, name_to_midi = build_note_dictionaries(note_names)
+	chords = notes_data["chords"]
 
-	midi_to_name_eng_8va, name_to_midi_eng_8va = build_note_dictionaries(english_names, True)
-	midi_to_name_fr, name_to_midi_fr = build_note_dictionaries(solfeggio_names, False)
-	print(midi_to_name_eng_8va[64])
-	print(name_to_midi_eng_8va["C0"])
-	print(midi_to_name_fr[61])
-	print(midi_to_name_fr[73])
-	print(name_to_midi_fr["Fa#"])
+	def foo0(midi_outputs):
+		print("henlo")
+	def foo1(midi_outputs):
+		print("k bye")
+	def sustain_on(midi_outputs):
+		msg = mido.Message("control_change", channel=0, control=64, value=127)
+		for o in midi_outputs:
+			o.send(msg)
+	def sustain_off(midi_outputs):
+		msg = mido.Message("control_change", channel=0, control=64, value=0)
+		for o in midi_outputs:
+			o.send(msg)
 
-	input("Appuyez sur ENTER pour passer à l'étape suivante...")
-	print("- - " * 30)
-
-	midi_to_name, name_to_midi = build_note_dictionaries(solfeggio_names, True)
-	print_note_name = build_print_note_name_callback(midi_to_name)
-	keyboard = mido.open_input(PORT_MIDI, callback=print_note_name)
-
-	input("Affichage des noms de notes (Appuyez sur ENTER pour passer à l'étape suivante)..." "\n")
-	keyboard.close()
-
-	print("- - " * 30)
-
-	chord_names = {
-		"Do majeur" : ("Do", "Mi", "Sol"),
-		"Fa majeur" : ("Fa", "La", "Do"),
-		"Sol majeur" : ("Sol", "Si", "Ré"),
-		"La mineur" : ("La", "Do", "Mi")
+	custom_actions = {
+		"foo": {True: foo0, False: foo1},
+		"sustain": {True: sustain_on, False: sustain_off}
 	}
-	
-	midi_to_name, name_to_midi = build_note_dictionaries(solfeggio_names, False)
-	print_chord_name = build_print_chord_name_callback(chord_names, name_to_midi)
-	keyboard = mido.open_input(PORT_MIDI, callback=print_chord_name)
-	
-	input("Affichage des noms d'accords (Appuyez sur ENTER pour passer à l'étape suivante)..." "\n")
-	keyboard.close()
+
+	mappings = load_input_mappings("input.ini", name_to_midi, chords, midi_outputs, custom_actions)
+
+	while True:
+		for e in gamepad.read():
+			btn = e.code.lower()
+			pressed = bool(e.state)
+			if btn in mappings:
+				callbacks = mappings[btn]
+				if pressed in callbacks and callbacks[pressed] is not None:
+					mappings[btn][pressed]()
+				print(btn, pressed)
 
 if __name__ == "__main__":
 	main()
